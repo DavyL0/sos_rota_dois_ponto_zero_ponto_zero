59542cb6d800fbc94a98dbac705cca6f49cd39d3
murilo <57834900+muriloonunes@users.noreply.github.com>
2026-06-01 19:37:14 -0300

    Commit inicial do projeto

---
+++ backend/SOS Rota/src/test/java/com/mhd/sosrota/SosRotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhd.sosrota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosRotaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
