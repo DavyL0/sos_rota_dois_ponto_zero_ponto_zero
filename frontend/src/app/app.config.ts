499126d0f1f457b8a9c701d78ad5edffa7aabb87
murilo <57834900+muriloonunes@users.noreply.github.com>
2026-06-01 19:44:26 -0300

    Adiciona arquivos iniciais do projeto

---
+++ frontend/src/app/app.config.ts
@@ -0,0 +1,17 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    providePrimeNG({
+      theme: {
+        preset: 'Aura',
+      },
+    }),
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+  ],
+};
