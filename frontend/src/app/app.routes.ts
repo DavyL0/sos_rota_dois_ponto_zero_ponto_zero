f06320ee1184fd54eb3a74fdbf59d75ff661148a
Murilo <57834900+muriloonunes@users.noreply.github.com>
2026-06-02 11:19:32 -0300

    feat: implementa a base do frontend: sidebar, css, html e as rotas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './screens/dashboard/dashboard';
+import { Ocorrencias } from './screens/ocorrencias/ocorrencias';
+import { Ambulancias } from './screens/ambulancias/ambulancias';
+import { Equipes } from './screens/equipes/equipes';
+import { Relatorios } from './screens/relatorios/relatorios';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Dashboard },
+  { path: 'ocorrencias', component: Ocorrencias },
+  { path: 'ambulancias', component: Ambulancias },
+  { path: 'equipes', component: Equipes },
+  { path: 'relatorios', component: Relatorios },
+];
---
+++ frontend/src/app/screens/ambulancias/ambulancias.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ambulancias',
+  imports: [],
+  templateUrl: './ambulancias.html',
+  styleUrl: './ambulancias.css',
+})
+export class Ambulancias {}
