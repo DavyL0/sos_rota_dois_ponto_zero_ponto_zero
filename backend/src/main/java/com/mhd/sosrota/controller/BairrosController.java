15ba5a28992c270975e04ed26eb22453a3d61ab2
Davy Lopes <d4vyl0pes@gmail.com>
2026-06-08 20:17:16 -0300

    fix: nome controller bairros

--- backend/src/main/java/com/mhd/sosrota/controller/BairrosController.java
+++ backend/src/main/java/com/mhd/sosrota/controller/BairrosController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/bairros")
+@RequestMapping("api/bairros")
 public class BairrosController {
 
     BairrosService bairrosService;
