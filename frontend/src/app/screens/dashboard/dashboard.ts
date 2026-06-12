e04f1deef067b74034717fe0e048a610a9190284
murilo <57834900+muriloonunes@users.noreply.github.com>
2026-06-12 15:42:31 -0300

    feat: termina de implementar o dashboard

--- frontend/src/app/screens/dashboard/dashboard.ts
+++ frontend/src/app/screens/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, inject, NgZone, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, NgZone, OnDestroy, OnInit } from '@angular/core';
 import { DashboardService } from '../../services/dashboard-service/dashboard-service';
 import { DashboardDados } from '../../model/dashboard.model';
 import { OcorrenciasService } from '../../services/ocorrencias-service/ocorrencias-service';
@@ -7,7 +7,7 @@ import {
   GravidadeOcorrenciaLabel,
   OcorrenciaExibicaoModel,
   StatusOcorrencia,
-  StatusOcorrenciaLabel
+  StatusOcorrenciaLabel,
 } from '../../model/ocorrencias.model';
 import { TableModule } from 'primeng/table';
 import { Skeleton } from 'primeng/skeleton';
@@ -16,24 +16,34 @@ import { Tag } from 'primeng/tag';
 import { NgClass } from '@angular/common';
 import { Tooltip } from 'primeng/tooltip';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
-import {
-  OcorrenciaDetalhesComponent
-} from '../../component/ocorrencia-detalhes-component/ocorrencia-detalhes-component';
+import { OcorrenciaDetalhesComponent } from '../../component/ocorrencia-detalhes-component/ocorrencia-detalhes-component';
 import { interval, Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { OcorrenciaCancelarComponent } from '../../component/ocorrencia-cancelar-component/ocorrencia-cancelar-component';
+import { Toast } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import {
+  DespacharOcorrenciaComponent
+} from '../../component/despachar-ocorrencia-component/despachar-ocorrencia-component';
+import {
+  OcorrenciaCadastrarComponent
+} from '../../component/ocorrencia-cadastrar-component/ocorrencia-cadastrar-component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [TableModule, Skeleton, Button, Tag, NgClass, Tooltip],
-  providers: [DialogService, DynamicDialogRef],
+  imports: [TableModule, Skeleton, Button, Tag, NgClass, Tooltip, Toast],
+  providers: [DialogService, DynamicDialogRef, MessageService],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard implements OnInit {
+export class Dashboard implements OnInit, OnDestroy {
+  private router = inject(Router);
   private dashboardService = inject(DashboardService);
   private ocorrenciasService = inject(OcorrenciasService);
   private cd = inject(ChangeDetectorRef);
   private ngZone = inject(NgZone);
   private ref: DynamicDialogRef | null = inject(DynamicDialogRef);
+  private messageService = inject(MessageService);
   private dialogService = inject(DialogService);
   private timerSubscription!: Subscription;
 
@@ -54,6 +64,16 @@ export class Dashboard implements OnInit {
       });
     });
 
+    this.carregarDados();
+  }
+
+  ngOnDestroy() {
+    if (this.timerSubscription) {
+      this.timerSubscription.unsubscribe();
+    }
+  }
+
+  private carregarDados() {
     this.dashboardService.carregarDashboard().subscribe({
       next: (dashboard) => {
         console.log(dashboard);
@@ -66,7 +86,7 @@ export class Dashboard implements OnInit {
     });
 
     this.carregandoOcorrencias = true;
-    this.ocorrenciasService.obterOcorrencias(0, 5).subscribe({
+    this.ocorrenciasService.obterOcorrencias(0, 10).subscribe({
       next: (dados) => {
         this.ocorrencias = dados.content;
         this.carregandoOcorrencias = false;
@@ -81,26 +101,45 @@ export class Dashboard implements OnInit {
   }
 
   protected cancelarOcorrencia(ocorrencia: OcorrenciaExibicaoModel) {
-    // this.ref = this.dialogService.open(OcorrenciaCancelarComponent, {
-    //   header: 'Cancelar Ocorrência',
-    //   width: '60vw',
-    //   modal: true,
-    //   closable: true,
-    //   inputValues: {
-    //     idEditando: ocorrencia.id,
-    //   },
-    // });
-    //
-    // this.ref?.onClose.subscribe((ocorrencia: OcorrenciaExibicaoModel | null) => {
-    //   if (ocorrencia) {
-    //     this.messageService.add({
-    //       severity: 'success',
-    //       summary: 'Sucesso',
-    //       detail: 'Ocorrência atualizada com sucesso',
-    //     });
-    //     this.carregarDados();
-    //   }
-    // });
+    this.ref = this.dialogService.open(OcorrenciaCancelarComponent, {
+      header: 'Cancelar Ocorrência',
+      width: '60vw',
+      modal: true,
+      closable: true,
+      inputValues: {
+        idEditando: ocorrencia.id,
+      },
+    });
+
+    this.ref?.onClose.subscribe((ocorrencia: OcorrenciaExibicaoModel | null) => {
+      if (ocorrencia) {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Sucesso',
+          detail: 'Ocorrência atualizada com sucesso',
+        });
+      }
+    });
+  }
+
+  abrirCadastrar() {
+    this.ref = this.dialogService.open(OcorrenciaCadastrarComponent, {
+      header: 'Adicionar nova ocorrência',
+      width: '60vw',
+      modal: true,
+      closable: true,
+    });
+
+    this.ref?.onClose.subscribe((result) => {
+      if (result) {
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Sucesso',
+          detail: 'Ocorrência criada com sucesso',
+        });
+        this.carregarDados();
+      }
+    });
   }
 
   abrirDetalhes(ocorrencia: OcorrenciaExibicaoModel) {
@@ -115,15 +154,15 @@ export class Dashboard implements OnInit {
   }
 
   abrirDespachar(ocorrencia: OcorrenciaExibicaoModel) {
-    // this.ref = this.dialogService.open(DespacharOcorrenciaComponent, {
-    //   header: 'Despachar ambulância',
-    //   width: '60vw',
-    //   modal: true,
-    //   closable: true,
-    //   inputValues: {
-    //     ocorrenciaDespacho: ocorrencia,
-    //   },
-    // });
+    this.ref = this.dialogService.open(DespacharOcorrenciaComponent, {
+      header: 'Despachar ambulância',
+      width: '60vw',
+      modal: true,
+      closable: true,
+      inputValues: {
+        ocorrenciaDespacho: ocorrencia,
+      },
+    });
   }
 
   getGravidadeLabel(gravidade: GravidadeOcorrencia): string {
@@ -172,6 +211,10 @@ export class Dashboard implements OnInit {
     const data = new Date(dataString);
 
     return data.toLocaleDateString('pt-BR');
+  }
+
+  verTodasOcorrencias() {
+    this.router.navigate(['/ocorrencias']);
   }
 
   atualizarSla() {
